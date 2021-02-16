ad9167f411df5432d71a01415ed9f1fdd9aa4668
Alan Libby <alan.h.libby@gmail.com>
2021-02-16 15:36:05 -0500

    Create simple driver to test Hero class

--- ToH-UI/Program.cs
+++ ToH-UI/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-
+using ToH_Models;
 namespace ToH_UI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hero hero = new Hero();
+            hero.HeroName = "A hero...";
+            Console.WriteLine("The hero's name is: " + hero.HeroName);
         }
     }
 }
