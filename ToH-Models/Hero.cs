3c45a4ad99dcd407f18df0b266718413db3c7f77
Alan Libby <alan.h.libby@gmail.com>
2021-02-16 15:32:59 -0500

    Add HeroName property to Hero class

--- ToH-Models/Hero.cs
+++ ToH-Models/Hero.cs
@@ -2,7 +2,22 @@
 
 namespace ToH_Models
 {
+    /// <summary>
+    /// Data structure used for modeling a hero
+    /// </summary>
     public class Hero
     {
+        private string heroName;
+
+        /* HeroName is a property (smart field) of the Hero class
+         * Its backing field is heroName (note capitalization)
+         * C# classes also have fields, methods, and constructors just like in Java.
+         */
+        public string HeroName { get { return heroName; } set { 
+            if (value.Equals(null)) {
+                throw new Exception("Hero name cannot be null.");
+            }
+            heroName = value;
+         } }
     }
 }
