b314348320155eac05a14ad22a67abaf9af7de84
Alan Libby <alan.h.libby@gmail.com>
2021-02-16 14:57:41 -0500

    Create Tour of Heroes app to follow demo

---
+++ ToH-UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToH_UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
